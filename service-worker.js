a647f80b11d13542bb6d9008d7d9718867ed294e
The-Wannabe-Scientist <293016143+The-Wannabe-Scientist@users.noreply.github.com>
2026-06-16 05:31:57 +0530

    cache ver update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "touch-assay-cache-v1.1.5.2";
+const CACHE_NAME = "touch-assay-cache-v1.1.5.4";
 
 // Ensure paths match your actual directory structure!
 const ASSETS_TO_CACHE = [
